0d9a08ca53a3d6c4d93a0270cfc3f55ae59bdc0d
devsrsihab <91025640+devsrsihab@users.noreply.github.com>
2022-04-03 16:17:29 +0600

    4/3/2022

---
+++ 904Class-2/README.md
@@ -0,0 +1,1 @@
+## Php Class 2 Assignment
---
+++ 904Class-2/assignment.php
@@ -0,0 +1,30 @@
+<?php
+
+// I have three old hp monitors, not new. I want to sell it on my own website. That’s why I should create a product upload form on my website, then upload my product with specification and available quantity. My monitors have a 15.6 inch display. My monitor’s VGA cable status, yes, I will sell it for 8000 tk.
+
+
+$monitor_condition = "old";//data type is string
+$monitor_name= "HP";//data type is string
+$monitor_inch= 15.6;//data type is float/double
+$monitor_cable_status = true;//data type is boolean
+$monitor_price = 8000;//data type is integer
+$how_many_monitor = 'Three';//data type is string 
+$product_quantites = 3;//data type is integer 
+
+
+
+//just try 
+/*echo      "	I have three {$monitor_condition} {$monitor_name} monitors, not new. I want to sell it on my own website. That’s why I should create a product upload form on my website, then upload my product with specification and available quantity. My monitors have a {$monitor_inch} inch display. My monitor’s VGA cable status, {$monitor_cable_status}, I will sell it for {$monitor_price} tk. <br> <br> " ;
+
+print   "	I have three {$monitor_condition} {$monitor_name} monitors, not new. I want to sell it on my own website. That’s why I should create a product upload form on my website, then upload my product with specification and available quantity. My monitors have a {$monitor_inch} inch display. My monitor’s VGA cable status, {$monitor_cable_status}, I will sell it for {$monitor_price} tk. <br> <br> " ;
+
+printf  ("	I have three %s %s  monitors, not new. I want to sell it on my own website. That’s why I should create a product upload form on my website, then upload my product with specification and available quantity. My monitors have a %s inch display. My monitor’s VGA cable status, %s , I will sell it for %d tk. <br> <br>", $monitor_condition, $monitor_name, $monitor_inch, $monitor_cable_status, $monitor_price) ;
+
+$result = sprintf("	I have three %s %s  monitors, not new. I want to sell it on my own website. That’s why I should create a product upload form on my website, then upload my product with specification and available quantity. My monitors have a %s inch display. My monitor’s VGA cable status, %s , I will sell it for %d tk.", $monitor_condition, $monitor_name, $monitor_inch, $monitor_cable_status, $monitor_price);
+
+echo $result*/
+
+
+
+
+?>
